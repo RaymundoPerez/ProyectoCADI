2301f7b573e3a2752810e102d3742807b3e9e197
RaymundoPerez <raymundo170@hotmail.com>
2017-05-31 18:53:44 -0500

    Se crearon las interfaces y clases DAO de Cuenta, UsuarioAutonomo y Empleado, asi como la interfaz de inicio de sesión.

---
+++ ProyectoCADI/src/Negocio/CuentaDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+/**
+ *
+ * @author raymu
+ */
+public class CuentaDAO implements ICuentaDAO{
+
+    @Override
+    public InformacionInicioSesion iniciarSesion(Cuenta cuenta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean cerrarSesión() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
